e3fb3c516c41487303bf21e98eb7c66cb7564a23
19048 <19048@DESKTOP-CSCAHNJ.attlocal.net>
2025-03-24 15:50:43 -0400

    Modified the file.

--- src/FindDuplicates.java
+++ src/FindDuplicates.java
@@ -9,7 +9,7 @@ public class FindDuplicates {
     public static void main(String[] args) {
       // List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 2, 5, 6, 1, 7, 8, 8);
     	List<Integer> numbers1 =Arrays.asList(3,4,5);
-    	List<String> numbers = Arrays.asList("arjun", "arjun", "Raju", "Raju","vijay", "kiran");
+    	List<String> numbers = Arrays.asList( "arjun", "Raju", "Raju","vijay", "kiran");
 
      Map<String,Long> numbersMap= numbers.stream()
                 .collect(Collectors.groupingBy(x->x, Collectors.counting()));
